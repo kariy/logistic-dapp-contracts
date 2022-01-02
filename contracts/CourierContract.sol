8b23acfee5eb6f4fcff58ad535485073ad21ff63
Ammar Arif <ammr.arf@gmail.com>
2022-01-02 22:57:11 +0800

    forwardItem function structure suggestion

--- contracts/CourierContract.sol
+++ contracts/CourierContract.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >= 0.7.0;
+pragma solidity >=0.7.0;
 import "./Ownable.sol";
 import "./ContainerCompany.sol";
 
 contract CourierContract {
+    uint256 item_count = 0;
+    // address ContainerContractAddress;
 
-    uint item_count = 0;
-    address ContainerContractAddress;
-
-    enum ItemStatus{
+    enum ItemStatus {
         Processing,
         Shipped,
         Ongoing,
@@ -16,15 +15,15 @@ contract CourierContract {
         LostInTransit
     }
 
-    enum ShipmentType{
+    enum ShipmentType {
         Domestic,
         International
     }
 
-    struct Location{
+    struct Location {
         string location_name;
-        uint long;
-        uint lat;
+        uint256 long;
+        uint256 lat;
     }
 
     // struct Destination{
@@ -32,47 +31,47 @@ contract CourierContract {
     //     Location location;
     // }
 
-    struct Checkpoints{
+    struct Checkpoints {
         string status;
         string desc;
         address operator;
-        Location location; 
-        uint timestamp; //https://ethereum.stackexchange.com/questions/32173/how-to-handle-dates-in-solidity-and-web3
+        Location location;
+        uint256 timestamp; //https://ethereum.stackexchange.com/questions/32173/how-to-handle-dates-in-solidity-and-web3
     }
 
-    struct Item{
-        uint id;
+    struct Item {
+        uint256 id;
         ShipmentType shipment;
         string destination;
         ItemStatus status;
         Checkpoints[] check_point;
-        uint date_created;
-        uint date_completed;
+        uint256 date_created;
+        uint256 date_completed;
         address forwarded_to;
-        uint price;
+        uint256 price;
     }
 
     //map the item
-    mapping(uint => Item) public _item;
+    mapping(uint256 => Item) public _item;
 
     function create_item(
-        uint _shipment,
+        uint256 _shipment,
         string memory _destination,
         //string memory _receiver_sign,
-        uint _price ) public {
-
+        uint256 _price
+    ) public {
         //make new item
         Item storage newItem = _item[item_count];
 
         //item id
-        newItem.id =  item_count;
+        newItem.id = item_count;
 
         //item shipment type
-        if(_shipment == 0){
+        if (_shipment == 0) {
             newItem.shipment = ShipmentType.Domestic;
-        }else if(_shipment == 1){
+        } else if (_shipment == 1) {
             newItem.shipment = ShipmentType.International;
-        }else{
+        } else {
             revert("Invalid input");
         }
 
@@ -86,21 +85,21 @@ contract CourierContract {
         newItem.date_created = block.timestamp;
 
         //newItem.receiver_sign = _receiver_sign;
-        
+
         newItem.price = _price;
         //increment count
         item_count++;
     }
 
     function addCheckpoint(
-        uint itemId,
+        uint256 itemId,
         string memory _status,
         string memory _desc,
         address _operator,
         string memory _Location_name,
-        uint _long,
-        uint _lat
-    )public{
+        uint256 _long,
+        uint256 _lat
+    ) public {
         //fill in location
         Location memory newLoc = Location(_Location_name, _long, _lat);
 
@@ -110,8 +109,9 @@ contract CourierContract {
             _desc, //description of item
             _operator, //address of sender
             newLoc, //input location
-            block.timestamp);//record timstamp
-        
+            block.timestamp
+        ); //record timstamp
+
         //push checkpoint according to itemid
         _item[itemId].check_point.push(newCheckPoints);
 
@@ -119,7 +119,7 @@ contract CourierContract {
         updateItemStatus(itemId, ItemStatus.Ongoing);
     }
 
-    function updateItemStatus(uint itemId, ItemStatus newStatus)public{
+    function updateItemStatus(uint256 itemId, ItemStatus newStatus) public {
         //update status based on itemid
         //all_items[itemId].status = newStatus;
         _item[itemId].status = newStatus;
@@ -133,30 +133,37 @@ contract CourierContract {
         string memory _locName,
         uint256 _long,
         uint256 _lat
-    ) public{
+    ) public {
         require(
             _item[_itemId].status == ItemStatus.Processing,
             "This container has already been shipped!"
         );
 
-    //betul ke ni?
-    addCheckpoint(_itemId, _status, _desc, msg.sender, _locName, _long, _lat);
-    updateItemStatus(_itemId, ItemStatus.Shipped);
+        //betul ke ni?
+        addCheckpoint(
+            _itemId,
+            _status,
+            _desc,
+            msg.sender,
+            _locName,
+            _long,
+            _lat
+        );
+        updateItemStatus(_itemId, ItemStatus.Shipped);
     }
 
     //transfer event
-    event Transfer(address to, uint amount, uint balance);
+    event Transfer(address to, uint256 amount, uint256 balance);
 
     //Completeshipment function
     function completeShipment(
-        uint _itemId,
+        uint256 _itemId,
         string memory _status,
         string memory _desc,
         string memory _location_name,
-        uint _long,
-        uint _lat
-        ) public payable{
-
+        uint256 _long,
+        uint256 _lat
+    ) public payable {
         // require(
         //     msg.sender == _item[itemId].destination.receiver,
         //     "Only the receiver of this item can complete the shipment!"
@@ -164,38 +171,56 @@ contract CourierContract {
         // require(
         //     _item.status != ItemStatus.Delivered,
         //     "The shipment of this container is already completed!"
-        // );   
-        
-        addCheckpoint(_itemId, _status, _desc, msg.sender, _location_name, _long, _lat);
+        // );
+
+        addCheckpoint(
+            _itemId,
+            _status,
+            _desc,
+            msg.sender,
+            _location_name,
+            _long,
+            _lat
+        );
         updateItemStatus(_itemId, ItemStatus.Delivered);
-        
-        if(msg.value < _item[_itemId].price){
-        revert("Transaction failed");
+
+        if (msg.value < _item[_itemId].price) {
+            revert("Transaction failed");
         }
 
         emit Transfer(msg.sender, msg.value, address(this).balance);
-          
+
         //payTo(msg.sender.address, price);
     }
-   
+
     // function transferEth(address payable recipient, uint itemId)external {
     //     recipient.transfer(_item[itemId].price);
     // }
-  
+
     //forward function
-    function setAddress(address _container_address) external{
-        //https://www.youtube.com/watch?v=YxU87o4U5iw
-        ContainerContractAddress = _container_address;
-    }
+    // function setAddress(address _container_address) external {
+    //     //https://www.youtube.com/watch?v=YxU87o4U5iw
+    //     ContainerContractAddress = _container_address;
+    // }
 
     // function call_queueItem(uint itemId) external{
-    //     ContainerCompany contract1 = ContainerCompany(ContainerContractAddress); 
+    //     ContainerCompany contract1 = ContainerCompany(ContainerContractAddress);
     //     //address(this)
     //     contract1.queueItem(_item[itemId].destination, address(this), itemId);
     // }
 
-    
-
-
-    
+    // pandai2 kau adjust
+    // yang aku buat tu basically, what the function should do
+    /// @param ...rest : just a placeholder kalau2 ada parameters lain yang perlu
+    function forwardItemToContainer(address containerAddress, uint256 itemId, ...rest)
+        external
+    {
+        // ContainerContract containerContract = ContainerContract(containerAddress)
+        // containerContract.queueItem(countryDest, address(this), itemId)
+        //
+        // _item[itemId].forwarded_to = containerAddress
+        //
+        // addCheckpoint()
+        // updateItemStatus()
+    }
 }
